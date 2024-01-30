1f3ac8b1100475e14140ad5fa5a2f36b6979ba87
ceyraokk <158149203+ceyraokk@users.noreply.github.com>
2024-01-30 17:21:38 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "OK! EDI TIISIN MO UGALI KO. I LOVE YOU SO MUCH BABY HEHE MWA!!";
+    gif.src = "https://media.tenor.com/sQCzvx3U2J8AAAAj/milk-and-mocha.gif";
+});
+
+noBtn.addEventListener("mouseover", () => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.TOP = randomY + "px";
+});
